7a81e1ed37f925271b5e2d159adc1b1e711a7413
gleegaye <32540036+gleegaye@users.noreply.github.com>
2020-03-08 23:59:51 +0100

    Add files via upload

---
+++ create-inventory.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+PROJECT_ID=$1
+gcloud config set project $PROJECT_ID
+cat > inventory.cfg <<EOF
+[all]
+$(gcloud compute instances list | grep -v NAME | awk '{ print $5 }')
+
+[workers]
+$(gcloud compute instances list --filter="(tags.items:kube-worker)" | grep -v NAME | awk '{ print $5 }')
+
+[masters]
+$(gcloud compute instances list --filter="(tags.items:kube-master)" | grep -v NAME | awk '{ print $5 }')
+
+[master]
+$(gcloud compute instances list --filter="(name:master)" | grep -v NAME | awk '{ print $5 }')
+EOF
